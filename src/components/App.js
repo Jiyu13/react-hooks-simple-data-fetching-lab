567b077623d35e54fa59689f9e1135213ac6cc89
Ziru Zou <jiyucoding@yahoo.com>
2023-02-06 03:46:00 -0500

    use useEffect to fetch an img, set img state and update setter after fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import { useEffect, useState } from "react";
+
+function App() {
+
+    const [img, setImg] = useState("")
+    // const [fetched, setFetched] = useState(false)
+
+    // const display = fetched ? "none" : ""
+    useEffect(() => {
+        
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(dog => {
+            setImg(dog.message)})
+    }, []
+    )
+
+    return <img src={img} alt="A Random Dog"/>
+    }
+
+export default App
